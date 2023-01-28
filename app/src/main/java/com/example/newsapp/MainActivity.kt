cd15d7e092c1e3f438625f14521aece18498ff34
Rubanov Andrey <nirwash@bk.ru>
2023-01-28 19:12:41 +0300

    Initial commit

---
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.newsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    private val KEY: STRING = "150432a6b07b42fb8286419d6eb74964"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
