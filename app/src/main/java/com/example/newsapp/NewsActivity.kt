d4dfe8f12e9b3cf0d141d193288508a7dcbfb6db
Rubanov Andrey <nirwash@bk.ru>
2023-01-28 19:18:57 +0300

    created ui

--- app/src/main/java/com/example/newsapp/NewsActivity.kt
+++ app/src/main/java/com/example/newsapp/NewsActivity.kt
@@ -3,10 +3,10 @@ package com.example.newsapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class NewsActivity : AppCompatActivity() {
     private val KEY: STRING = "150432a6b07b42fb8286419d6eb74964"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_news)
     }
 }
