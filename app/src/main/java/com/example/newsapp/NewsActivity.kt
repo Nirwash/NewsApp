2e6444a4658b8042deaa0158d7b5e6f1d606dd15
Rubanov Andrey <nirwash@bk.ru>
2023-01-28 19:19:27 +0300

    refactor

--- app/src/main/java/com/example/newsapp/NewsActivity.kt
+++ app/src/main/java/com/example/newsapp/NewsActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class NewsActivity : AppCompatActivity() {
-    private val KEY: STRING = "150432a6b07b42fb8286419d6eb74964"
+    private val KEY: String = "150432a6b07b42fb8286419d6eb74964"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
